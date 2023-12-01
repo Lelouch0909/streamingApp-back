9455c78c698c3ef704f02af6da5a3442213c3d38
Lelouch0909 <lnhm87404@gmail.com>
2023-12-01 08:34:51 +0100

    new file:   .gitignore new file:   .mvn/wrapper/maven-wrapper.jar new file:   .mvn/wrapper/maven-wrapper.properties new file:   mvnw new file:   mvnw.cmd new file:   pom.xml new file:   src/main/java/dev/lontsi/movies/Movie.java new file:   src/main/java/dev/lontsi/movies/MovieController.java new file:   src/main/java/dev/lontsi/movies/MovieRepository.java new file:   src/main/java/dev/lontsi/movies/MovieService.java new file:   src/main/java/dev/lontsi/movies/MoviesApplication.java new file:   src/main/java/dev/lontsi/movies/Review.java new file:   src/main/java/dev/lontsi/movies/playground-2.mongodb.js new file:   src/main/resources/application.properties new file:   src/main/resources/playground-1.mongodb.js new file:   src/test/java/dev/lontsi/movies/MoviesApplicationTests.java

---
+++ src/main/java/dev/lontsi/movies/Review.java
@@ -0,0 +1,19 @@
+package dev.lontsi.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    
+    private ObjectId id;
+
+    private String body;
+}
---
+++ src/test/java/dev/lontsi/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lontsi.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
